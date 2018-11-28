2e777b34aac5c138091adaf17f7cde81c5d4690f
suweya <suweya89@gmail.com>
2018-11-28 15:36:12 +0800

    impl code input style

--- example/src/App.js
+++ example/src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 
 import ReactCodeInput from 'react-code-input';
 
+const STYLE = {
+  width: '350px',
+  margin: '50px auto'
+}
+
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div style={STYLE}>
         <ReactCodeInput
           fields={5}
           onChange={v => console.log(v)}
